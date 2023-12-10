3893846d86cbc9b28581e01975540872fac48823
Gabriel Florit <gabrielflorit@gmail.com>
2023-12-09 23:20:28 -0500

    Initial layout

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import type { LinksFunction } from "@remix-run/cloudflare";
-import { cssBundleHref } from "@remix-run/css-bundle";
+// import { cssBundleHref } from "@remix-run/css-bundle";
 import {
   Links,
   LiveReload,
@@ -9,8 +9,12 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import styles from "./tailwind.css";
+
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: styles },
+
+  // ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
 export default function App() {
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,73 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import type { MetaFunction, LinksFunction } from "@remix-run/cloudflare";
+import styles from "~/styles/index.css";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Gabriel Florit" },
+    { name: "description", content: "Gabriel Florit" },
   ];
 };
 
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <article>
+      <h2>
+        Barcelona - Lima
+        <p></p>
+      </h2>
+      <header>
+        <p>
+          here goes text and some more and some more and maybe some more and
+          some more because it should be two lines
+        </p>
+        <h2>Gabriel Florit</h2>
+        <p>here goes text and some more and some more and maybe some more</p>
+        <h1>The new compendium</h1>
+        <h3>
+          A web log of mathematics, computational geometry, semiology of
+          graphics, and visual design
+        </h3>
+      </header>
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum
+          odio. Iaculis nunc sed augue lacus viverra vitae congue eu. Lorem
+          ipsum dolor sit amet consectetur. Tortor id aliquet lectus proin nibh
+          nisl condimentum. Lorem dolor sed viverra ipsum. Integer enim neque
+          volutpat ac tincidunt. Est ante in nibh mauris cursus mattis molestie
+          a. Aliquam malesuada bibendum arcu vitae elementum curabitur vitae
+          nunc. Pellentesque nec nam aliquam sem. Suspendisse faucibus interdum
+          posuere lorem ipsum dolor sit. Viverra ipsum nunc aliquet bibendum.
+        </p>
+        <p>
+          Neque gravida in fermentum et sollicitudin ac orci phasellus egestas.
+          Lectus urna duis convallis convallis tellus id interdum. Platea
+          dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Ipsum
+          dolor sit amet consectetur. In fermentum et sollicitudin ac orci
+          phasellus egestas tellus rutrum. Erat nam at lectus urna duis
+          convallis. Auctor augue mauris augue neque. Justo eget magna fermentum
+          iaculis eu non. Amet venenatis urna cursus eget nunc scelerisque
+          viverra mauris in. Facilisis volutpat est velit egestas dui. Duis
+          ultricies lacus sed turpis tincidunt id aliquet risus. Auctor augue
+          mauris augue neque gravida in fermentum. Risus ultricies tristique
+          nulla aliquet enim. Sit amet purus gravida quis blandit turpis cursus
+          in.
+        </p>
+      </section>
+
+      <nav>
+        <h3>Table of contents</h3>
+        <ul>
+          <li>one</li>
+          <li>two</li>
+          <li>three</li>
+        </ul>
+        <span></span>
+      </nav>
+    </article>
   );
 }
